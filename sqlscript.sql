ab14beee28307a735d50187b333c40b2bae315a0
Marcello <62134093+Moriarty2002@users.noreply.github.com>
2023-12-08 00:29:44 +0100

    Add oracle sql live script

---
+++ sqlscript.sql
@@ -0,0 +1,345 @@
+REM   Script: Electricity Theft pre procedure
+REM   Chianese
+
+-- Crea tabella Fornitore
+CREATE TABLE Fornitore(  
+    p_iva int,  
+    Nome VARCHAR(50) NOT NULL,  
+    CONSTRAINT pk_fornitore PRIMARY KEY(p_iva)  
+);
+
+-- Crea tabella Posizione
+CREATE TABLE Posizione(   
+    Provincia   VARCHAR(2),   
+    Regione     VARCHAR(30) NOT NULL,   
+    CONSTRAINT pk_posizione PRIMARY KEY(Provincia)   
+);
+
+-- Crea tabella Persona
+CREATE TABLE Persona(  
+    IdPersona   INT,  
+    NickName    VARCHAR(30), 
+    Nome        VARCHAR(30) NOT NULL,  
+    Cognome     VARCHAR(30) NOT NULL,  
+    TipologiaUtente CHAR(1) NOT NULL,  
+    Password    VARCHAR(64), -- for SHA-256 
+    CONSTRAINT pk_persona PRIMARY KEY(IdPersona)  
+);
+
+-- Crea tabella Bolletta
+CREATE TABLE Bolletta(  
+    CodUtente   INT,  
+    Prezzo  DECIMAL NOT NULL,  
+    Consumo DECIMAL NOT NULL,  
+    Mese    INT NOT NULL,  
+    Anno    INT NOT NULL,  
+    Attiva  CHAR(1) NOT NULL,  
+    Fornitore   INT,  
+    Persona     INT,  
+    Posizione   VARCHAR(2) NOT NULL,  
+    URL	VARCHAR(32), 
+    CONSTRAINT pk_bolletta PRIMARY KEY(CodUtente, Mese, Anno),  
+    CONSTRAINT fk_fornitore FOREIGN KEY(Fornitore) REFERENCES Fornitore(p_iva),  
+    CONSTRAINT fk_persona FOREIGN KEY(Persona) REFERENCES Persona(IdPersona),  
+    CONSTRAINT fk_posizione FOREIGN KEY(Posizione) REFERENCES Posizione(Provincia),
+    CONSTRAINT k_mese CHECK(Mese < 13 AND Mese > 0),
+    CONSTRAINT k_anno CHECK(Anno > 0)
+);
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CH', 'Abruzzo');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('AQ', 'Abruzzo');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PE', 'Abruzzo');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('TE', 'Abruzzo');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('AG', 'Sicilia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CL', 'Sicilia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CT', 'Sicilia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('EN', 'Sicilia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('ME', 'Sicilia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PA', 'Sicilia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('RG', 'Sicilia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('SR', 'Sicilia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('TP', 'Sicilia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PZ', 'Basilicata');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('MT', 'Basilicata');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CS', 'Calabria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('KR', 'Calabria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('RC', 'Calabria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('VV', 'Calabria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CZ', 'Calabria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('NA', 'Campania');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('AV', 'Campania');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('BN', 'Campania');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('SA', 'Campania');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CE', 'Campania');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('BO', 'Emilia-Romagna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('FE', 'Emilia-Romagna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('FC', 'Emilia-Romagna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('MO', 'Emilia-Romagna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PR', 'Emilia-Romagna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PC', 'Emilia-Romagna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('RA', 'Emilia-Romagna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('RE', 'Emilia-Romagna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('RN', 'Emilia-Romagna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('GO', 'Friuli-Venezia-Giulia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PN', 'Friuli-Venezia-Giulia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('TS', 'Friuli-Venezia-Giulia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('UD', 'Friuli-Venezia-Giulia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('RM', 'Lazio');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('FR', 'Lazio');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('LT', 'Lazio');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('RI', 'Lazio');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('VT', 'Lazio');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('GE', 'Liguria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('IM', 'Liguria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('SP', 'Liguria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('SV', 'Liguria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('MI', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('BG', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('BS', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CO', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CR', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('LC', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('LO', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('MB', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PV', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('SO', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('VA', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('MR', 'Lombardia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('AN', 'Marche');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('AP', 'Marche');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('FM', 'Marche');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('MC', 'Marche');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PU', 'Marche');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CB', 'Molise');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('IS', 'Molise');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('AL', 'Piemonte');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('AT', 'Piemonte');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('BI', 'Piemonte');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CN', 'Piemonte');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('NO', 'Piemonte');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('TO', 'Piemonte');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('VB', 'Piemonte');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('VC', 'Piemonte');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('BA', 'Puglia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('BT', 'Puglia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('BR', 'Puglia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('FG', 'Puglia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('LE', 'Puglia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('TA', 'Puglia');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('CA', 'Sardegna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('NU', 'Sardegna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('OR', 'Sardegna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('SS', 'Sardegna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('SU', 'Sardegna');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('AR', 'Toscana');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('FI', 'Toscana');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('GR', 'Toscana');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('LI', 'Toscana');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('LU', 'Toscana');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('MS', 'Toscana');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PI', 'Toscana');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PT', 'Toscana');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PO', 'Toscana');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('SI', 'Toscana');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('BZ', 'Trentino Alto Adige');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('TN', 'Trentino Alto Adige');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PG', 'Umbria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('TR', 'Umbria');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('AO', 'Valle d*Aosta');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('BL', 'Veneto');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('PD', 'Veneto');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('RO', 'Veneto');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('TV', 'Veneto');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('VE', 'Veneto');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('VR', 'Veneto');
+
+INSERT INTO Posizione(Provincia, Regione) VALUES ('VI', 'Veneto');
+
+-- SEQUENCE PERSONA
+CREATE SEQUENCE seq_persona 
+INCREMENT BY 1 
+START WITH 1 
+NOMAXVALUE 
+NOMINVALUE 
+NOCYCLE 
+NOCACHE;
+
+INSERT INTO PERSONA VALUES (seq_persona.NEXTVAL, 'nick', 'name', 'surname', 0, 'SHA-256');
+
+INSERT INTO FORNITORE VALUES (1, 'enel');
+
+INSERT INTO FORNITORE VALUES (2, 'Fastweb');
+
+-- VIEW COSTI
+CREATE MATERIALIZED VIEW costi AS  
+SELECT p.REGIONE, p.PROVINCIA, f.NOME, b.MESE, b.ANNO, AVG(b.PREZZO)/avg(b.CONSUMO) AS COSTO_MEDIO_KWH  
+FROM BOLLETTA b  
+JOIN POSIZIONE p ON b.POSIZIONE = p.PROVINCIA  
+JOIN FORNITORE f ON b.FORNITORE = f.P_IVA  
+GROUP BY p.REGIONE, p.PROVINCIA, f.NOME, b.MESE, b.ANNO 
+ORDER BY p.REGIONE ASC, p.PROVINCIA ASC;
+
+-- VIEW REGIONI
+CREATE MATERIALIZED VIEW REGIONI AS 
+SELECT REGIONE 
+FROM POSIZIONE 
+GROUP BY REGIONE 
+ORDER BY REGIONE ASC;
+
+-- PROCEDURE ATTIVAZIONE BOLLETTA
+CREATE OR REPLACE PROCEDURE attivazione_bolletta
+IS
+	CURSOR curs IS SELECT * FROM BOLLETTA WHERE ATTIVA = 'N';
+	riga curs%ROWTYPE;
+BEGIN
+	OPEN curs;
+
+	LOOP
+		FETCH curs INTO riga;
+		EXIT WHEN curs%NOTFOUND;
+
+		UPDATE BOLLETTA
+		SET ATTIVA = 'Y'
+		WHERE CODUTENTE = riga.CODUTENTE AND MESE = riga.MESE AND ANNO = riga.ANNO;
+
+		DBMS_OUTPUT.PUT_LINE('Aggiornata bolletta: ' || riga.CODUTENTE);
+	END LOOP;
+
+	CLOSE curs;
+END;
+
+
+-- PROCEDURE RIMOZIONE BOLLETTE
+CREATE OR REPLACE PROCEDURE elimina_record IS 
+	CURSOR curs IS SELECT * FROM BOLLETTA WHERE to_char( sysdate, 'mm' ) > to_char(MESE) AND to_char( sysdate, 'yy' ) > to_char(ANNO); 
+	riga curs%ROWTYPE; 
+BEGIN 
+	OPEN curs; 
+ 
+	LOOP 
+    	FETCH curs INTO riga; 
+    	EXIT WHEN curs%NOTFOUND; 
+ 
+    	DELETE FROM BOLLETTA WHERE riga.CODUTENTE = CODUTENTE AND riga.MESE = MESE AND riga.ANNO = ANNO; 
+ 
+	END LOOP; 
+END; 
+
+
+-- Trigger update MVIEW costi
+CREATE OR REPLACE TRIGGER trg_update_cost
+AFTER UPDATE ON BOLLETTA
+DECLARE
+    PRAGMA AUTONOMOUS_TRANSACTION;
+BEGIN
+	DBMS_MVIEW.REFRESH('costi');
+	COMMIT;
+END;
+
+
+
